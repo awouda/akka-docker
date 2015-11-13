7dee7acabff638aaae68041cc5e9c0d415f473f5
Remko de Jong <remko.de.jong@ordina.nl>
2015-11-13 23:03:52 +0100

    fixed build script and simplified Dockerfile

--- build-docker-server.sh
+++ build-docker-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sbt "project akkaCalc" assembly
-cp /akka-calc/target/scala-2.11/akka-calc-assembly-1.0.jar dockerServer/
+cp akka-calc/target/scala-2.11/akka-calc-assembly-1.0.jar dockerServer/
 docker rmi wojo:calc
 docker build -t wojo:calc dockerServer/
 

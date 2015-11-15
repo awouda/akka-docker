1dfad7ce7d23a40cb49604971d596c4328ac9b61
Alex Wouda <awouda@gmail.com>
2015-11-15 12:52:42 +0100

    akka instance in docker now gets correct ip

--- dockerServer/start.sh
+++ dockerServer/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-java -Dconfig.file=dock.conf -jar akka-calc-assembly-1.0.jar 
+REMOTE_IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+
+java -Dconfig.file=dock.conf -DdockerIp=$REMOTE_IP -jar akka-calc-assembly-1.0.jar 
 
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 #docker run --name calc -ti -p 9000:9001 wojo:calc java -Dconfig.file=dock.conf -jar akka-calc-assembly-1.0.jar
 
-docker run --name calc -ti -p 9100:2551 wojo:calc /bin/bash
+docker  run  --name calc -ti -p 9100:9100 wojo:calc /bin/bash

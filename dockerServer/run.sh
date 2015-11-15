f63eaf0af1c8ff8ffcc141b0f80f6ab7c8dfd50a
Alex Wouda <awouda@gmail.com>
2015-11-15 20:36:09 +0100

    now runs as daemon

--- dockerServer/run.sh
+++ dockerServer/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker rm calc
-docker  run  --name calc -ti -p 9100:9100 wojo:calc 
+docker  run  --name calc -d -p 9100:9100 wojo:calc 
--- server-build-and-run.sh
+++ server-build-and-run.sh
@@ -2,7 +2,7 @@
 
 sbt "project akkaCalc" assembly
 cp akka-calc/target/scala-2.11/akka-calc-assembly-1.0.jar dockerServer/
-docker rmi wojo:calc
+docker -f rmi wojo:calc
 docker build -t wojo:calc dockerServer/
 
 

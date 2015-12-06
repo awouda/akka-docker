c810bd881a84ab4b2ca58cbe883a95438012e5ad
Alex Wouda <awouda@gmail.com>
2015-12-06 20:08:32 +0100

    now calc server logs to file

--- akka-calc/build.sbt
+++ akka-calc/build.sbt
@@ -7,8 +7,10 @@ scalaVersion := "2.11.7"
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.4.0",
    "com.typesafe.akka" %% "akka-testkit" % "2.4.0",
-  "com.typesafe.akka" %% "akka-remote" % "2.4.0"
-)
+  "com.typesafe.akka" %% "akka-remote" % "2.4.0",
+  "com.typesafe.akka" %% "akka-slf4j" % "2.4.0",
+  "ch.qos.logback" % "logback-core" % "1.1.2",
+  "ch.qos.logback" % "logback-classic" % "1.1.2")
 
 
 
--- akka-calc/src/main/scala/com/jtmconsultancy/calculator/actor/CalculatorActor.scala
+++ akka-calc/src/main/scala/com/jtmconsultancy/calculator/actor/CalculatorActor.scala
@@ -1,14 +1,16 @@
 package com.jtmconsultancy.calculator.actor
 
-import akka.actor.Actor
+import akka.actor.{ActorLogging, Actor}
 import com.jtmconsultancy.messages._
 
 
-class CalculatorActor extends Actor {
+class CalculatorActor extends Actor with ActorLogging {
+
+  log.info("Calculator started")
 
   override def receive = {
-    case Start => println("Calculator started")
-    case Stop => println("stopping system")
+    case Start => log.info("Start message received ")
+    case Stop => log.info("stop message received")
     case Add(i,j) =>       sender ! CalcResult("Add",i+j)
     case Multiply(i,j) => sender ! CalcResult("Multiply",i*j)
   }
